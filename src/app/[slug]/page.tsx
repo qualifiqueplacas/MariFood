d349b4d59710326c7969913d7fde59530cd7fb0a
qualifiqueplacas <gabrielrannier31@gmail.com>
2025-09-26 14:58:15 -0300

    feat: add product details page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -31,7 +31,7 @@ const RestaurantPage = async ({ params }: RestaurantPageProps) => {
       <div className="space-y-2 pt-24 text-center">
         <h3 className="text-2xl font-semibold">Seja bem-vindo!</h3>
         <p className="opacity-55">
-          Escolha cmo prefere aproveitar sua refeição. Estamos aqui para
+          Escolha como prefere aproveitar sua refeição. Estamos aqui para
           oferecer praticidade e sabor em cada detalhe
         </p>
       </div>
