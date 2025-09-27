92995a5b94ac4605979fde8df110c969f0996334
qualifiqueplacas <gabrielrannier31@gmail.com>
2025-09-27 07:56:22 -0300

    feat: add initial add to cart function to cart context

--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -5,7 +5,7 @@ import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "
 import { CartContext } from "../contexts/cart";
 
 const CartSheet = () => {
-    const { isOpen, toggleCart} = useContext(CartContext);
+    const { isOpen, toggleCart, products } = useContext(CartContext);
     return ( 
             <Sheet open={isOpen} onOpenChange={toggleCart}>
                 <SheetContent>
@@ -16,6 +16,9 @@ const CartSheet = () => {
                                 and remove your data from our servers.
                             </SheetDescription>
                     </SheetHeader>
+                    {products.map(product => (
+                        <h1 key={product.id}>{product.name} - {product.quantity}</h1>
+                    ))}
                 </SheetContent>
             </Sheet>             
      );
